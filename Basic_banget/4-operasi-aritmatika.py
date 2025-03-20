6e48adaa7bdc0996adc973422abf1c8c54526214
Wahyu Azizi <wahyuazizi03@gmail.com>
2025-03-20 22:43:27 +0800

    belajar operasi aritmatika lagi

---
+++ Basic_banget/4-operasi-aritmatika.py
@@ -0,0 +1,36 @@
+# Operasi aritmatika
+
+a = 15
+b = 7
+
+# Operasi tambah
+hasil = a + b
+print(f"s + b = {hasil}")
+
+# Operasi pengurangan
+hasil = a - b
+print(f"s - b = {hasil}")
+
+# Operasi peerkalian
+hasil = a * b
+print(f"s * b = {hasil}")
+
+# Operasi peembagian
+hasil = a / b
+print(f"s / b = {hasil}")
+
+# Operasi modulus
+hasil = a % b
+print(f"s % b = {hasil}")
+
+# Operasi floor division
+hasil = a // b
+print(f"s // b = {hasil}")
+
+# Prioritas operasi
+x = 4
+y = 2
+z = 1
+
+hasil = x ** y * z + x / y - y %z // x
+print(f"hasil: {hasil}")
